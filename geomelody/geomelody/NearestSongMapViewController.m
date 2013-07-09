96d26ca842cc65713410a8557494a2d3482dc2dd
Milot Mirdita <milot@mirdita.de>
2013-07-09 19:42:24 +0200

    Changed zoom level

--- geomelody/geomelody/NearestSongMapViewController.m
+++ geomelody/geomelody/NearestSongMapViewController.m
@@ -91,6 +91,8 @@
     if([_mapView.annotations count] == 0)
         return;
     
+    int currentTrack = [delegate getCurrentTrackIndex];
+    
     CLLocationCoordinate2D topLeftCoord;
     topLeftCoord.latitude = -90;
     topLeftCoord.longitude = 180;
@@ -99,20 +101,24 @@
     bottomRightCoord.latitude = 90;
     bottomRightCoord.longitude = -180;
     
-    for(MapAnnotation* annotation in _mapView.annotations)
+    for(id<MKAnnotation> annotation in _mapView.annotations)
     {
-        topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
-        topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
-        
-        bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
-        bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
+        if([annotation isKindOfClass:[MapAnnotation class]]) {
+            if ([[(MapAnnotation*) annotation index] intValue] == currentTrack){
+                topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
+                topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
+                
+                bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
+                bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
+            }
+        }
     }
     
     MKCoordinateRegion region;
     region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
     region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
-    region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.1; // Add a little extra space on the sides
-    region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.1; // Add a little extra space on the sides
+    region.span.latitudeDelta = 0.01; // Add a little extra space on the sides
+    region.span.longitudeDelta = 0.01; // Add a little extra space on the sides
     
     region = [_mapView regionThatFits:region];
     [_mapView setRegion:region animated:YES];
